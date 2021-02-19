20bcd18457c93749092c12de455a846c57703019
DanielsKilLs20 <64350122+DanielsKilLs20@users.noreply.github.com>
2021-02-19 10:10:05 +0100

    js-sprawdzenie-liczby

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let num1 = 'n';
+
+function wynik(num1){
+    if (num1 > 100){
+        return 'Liczba znajduje się w przedziale';
+    } else if (num1 < 200){
+        return 'Liczba znajduje się w przedziale';
+    } else {
+        return 'Liczba nie znajduje się w przedziale ';
+    }
+}
+console.log(wynik(num1));
+
