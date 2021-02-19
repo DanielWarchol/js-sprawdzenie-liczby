e93b42f61afa26b23ff09138e99e936575facd61
DanielsKilLs20 <64350122+DanielsKilLs20@users.noreply.github.com>
2021-02-19 11:16:30 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let num1 = 100;
 
  function wynik(num1){
-     if (num1 > 100 && num1 < 200){
+     if (num1 > 100 && num1 <=200){
          return 'Liczba znajduje się w przedziale';
      } else if (num1 < 200){
          return 'Liczba znajduje się w przedziale';
