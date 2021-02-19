fb945c789a8ed744f220306c98023f4ef4f1b161
DanielsKilLs20 <64350122+DanielsKilLs20@users.noreply.github.com>
2021-02-19 11:12:35 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
-let num1 = 'n';
+let num1 = 100;
 
-function wynik(num1){
-    if (num1 > 100){
-        return 'Liczba znajduje się w przedziale';
-    } else if (num1 < 200){
-        return 'Liczba znajduje się w przedziale';
-    } else {
-        return 'Liczba nie znajduje się w przedziale ';
-    }
+ function wynik(num1){
+     if (num1 > 100 && num1 < 200){
+         return 'Liczba znajduje się w przedziale';
+     } else if (num1 < 200){
+         return 'Liczba znajduje się w przedziale';
+     } else {
+         return 'Liczba nie znajduje się w przedziale ';
+     }
 }
 console.log(wynik(num1));
+console.log(wynik(200));
 
